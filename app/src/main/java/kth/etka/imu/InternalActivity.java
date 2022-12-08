a04d9397bbe95aae5ecdc37f7162df8410a408f5
Eyrún <Eyrn Tanja>
2022-12-08 10:53:35 +0100

    haha

--- app/src/main/java/kth/etka/imu/InternalActivity.java
+++ app/src/main/java/kth/etka/imu/InternalActivity.java
@@ -2,13 +2,22 @@ package kth.etka.imu;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
+import android.hardware.SensorPrivacyManager;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class InternalActivity extends AppCompatActivity {
 
     TextView IMU, Internal;
-    //IMU.setText(String.valueOf(imu));
+    SensorManager sensorManager;
+
+
+    //IMU.setText(String.valueOf(imu)); //To change the value of the textview in the HTML
     //Internal.setText(String.valueOf(internal));
 
     int imu = 0;
@@ -19,6 +28,12 @@ public class InternalActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_internal);
 
-        //TODO: everything
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+
+        List<Sensor> accelerometer = sensorManager.getSensorList(Sensor.TYPE_ACCELEROMETER);
+        List<Sensor> gyroscope = sensorManager.getSensorList(Sensor.TYPE_GYROSCOPE);
+
+        //TODO: First method, ONLY accelerometer
+        //TODO: Second method, accelerometer and gyroscope
     }
 }
